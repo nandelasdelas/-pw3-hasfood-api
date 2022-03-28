c6e587b762788893e76eeaf5f382912f447ca165
nandelasdelas <nandinhotec@gmail.com>
2022-03-28 08:16:57 -0300

    Criacao do projeto

---
+++ src/test/java/br/com/luis/carneiro/hasfood/Pw3HasfoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.luis.carneiro.hasfood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pw3HasfoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
